26abcdd87a386e4b55c7bd674a7a398380b0eb31
hws1123 <ts24057@ts.hs.kr>
2025-06-20 02:01:34 +0000

    1

--- app_eda.py
+++ app_eda.py
@@ -47,21 +47,7 @@ class Home:
             st.success(f"{st.session_state.get('user_email')}님 환영합니다.")
 
         # Kaggle 데이터셋 출처 및 소개
-        st.markdown("""
-                ---
-                **Bike Sharing Demand 데이터셋**  
-                - 제공처: [Kaggle Bike Sharing Demand Competition](https://www.kaggle.com/c/bike-sharing-demand)  
-                - 설명: 2011–2012년 캘리포니아 주의 수도인 미국 워싱턴 D.C. 인근 도시에서 시간별 자전거 대여량을 기록한 데이터  
-                - 주요 변수:  
-                  - `datetime`: 날짜 및 시간  
-                  - `season`: 계절  
-                  - `holiday`: 공휴일 여부  
-                  - `workingday`: 근무일 여부  
-                  - `weather`: 날씨 상태  
-                  - `temp`, `atemp`: 기온 및 체감온도  
-                  - `humidity`, `windspeed`: 습도 및 풍속  
-                  - `casual`, `registered`, `count`: 비등록·등록·전체 대여 횟수  
-                """)
+        st.markdown("")
 
 # ---------------------
 # 로그인 페이지 클래스
